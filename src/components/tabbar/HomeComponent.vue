2e630b5a737d2486168504bde852f2b8902ee2af
yujiangling <yujiangling@qq.com>
2018-01-03 20:25:11 +0800

    轮播图完成

--- src/components/tabbar/HomeComponent.vue
+++ src/components/tabbar/HomeComponent.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
-        <h1>这是 HomeComponent 模块</h1>
+        <!-- 轮播图区域 -->
+        <mt-swipe :auto="4000">
+            <mt-swipe-item v-for="item in bannerimg" :key="item.img">
+                <img :src="item.img" alt="">
+            </mt-swipe-item>
+        </mt-swipe>
 
     </div>
 </template>
 
 <script>
+
+    import { Toast } from 'mint-ui'
+    
+    export default {
+        data(){
+            return {
+                bannerimg: []
+            }
+        },
+        created(){
+            this.getBannerImg();
+        },
+        methods: {
+            getBannerImg(){
+                this.$http.get("http://vue.studyit.io/api/getlunbo").then( result => {
+                    if(result.body.status == 0 ){
+                        this.bannerimg = result.body.message;
+                        console.log(result.body.message);
+                    }else {
+                        Toast('获取轮播图失败....');
+                    }
+                })
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
